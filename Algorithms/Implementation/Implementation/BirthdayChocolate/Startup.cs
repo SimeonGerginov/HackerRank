ac2e212cd5e41d81ae1b1dded028f5b20410a5b6
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-02-28 20:14:38 +0200

    Added BirthdayChocolate and BreakingTheRecords tasks

---
+++ Algorithms/Implementation/Implementation/BirthdayChocolate/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace BirthdayChocolate
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        private static int CountDifferentWaysToGiveChocolate(int[] pieces, int N, int days, int months)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int sum = pieces.Skip(i).Take(months).Sum();
+
+                if (sum == days)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] chocolatePieces = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] daysAndMonths = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int days = daysAndMonths[0];
+            int months = daysAndMonths[1];
+
+            int countOfDifferentWays = CountDifferentWaysToGiveChocolate(chocolatePieces, N, days, months);
+            Console.WriteLine(countOfDifferentWays);
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/BreakingTheRecords/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace BreakingTheRecords
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        private static int[] countTimesOfBreakingRecord(int[] scores, int N)
+        {
+            int[] result = new int[2];
+            int countOfBreakingBestScore = 0;
+            int countOfBreakingWorstScore = 0;
+            int currentBestScore = scores[0];
+            int currentWorstScore = scores[0];
+
+            for (int i = 1; i < N; i++)
+            {
+                if (scores[i] > currentBestScore)
+                {
+                    currentBestScore = scores[i];
+                    countOfBreakingBestScore++;
+                }
+
+                if(scores[i] < currentWorstScore)
+                {
+                    currentWorstScore = scores[i];
+                    countOfBreakingWorstScore++;
+                }
+            }
+
+            result[0] = countOfBreakingBestScore;
+            result[1] = countOfBreakingWorstScore;
+            return result;
+        }
+
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] scores = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] result = countTimesOfBreakingRecord(scores, N);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
