b3351c8a3a126eaccf27e9e63a5ced6f9a060916
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-03-22 11:11:27 +0200

    Added NonDivisibleSubset and RepeatedString tasks

---
+++ Algorithms/Implementation/Implementation/NonDivisibleSubset/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace NonDivisibleSubset
+{
+    public class Startup
+    {
+        private static int FindSizeOfSubset(int countOfNumbers, int divisor, int[] numbers)
+        {
+            int[] remainders = new int[divisor];
+            int counter = 0;
+
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                int index = numbers[i] % divisor;
+                remainders[index]++;
+            }
+
+            for (int i = 1; i <= divisor / 2; i++)
+            {
+                if (i != divisor - i)
+                {
+                    counter += Math.Max(remainders[i], remainders[divisor - i]);
+                }
+                else
+                {
+                    counter += 1;
+                }
+            }
+
+            if (remainders[0] > 0)
+            {
+                counter++;
+            }
+
+            return counter;
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int countOfNumbers = input[0];
+            int divisor = input[1];
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sizeOfLargestSubset = FindSizeOfSubset(countOfNumbers, divisor, numbers);
+            Console.WriteLine(sizeOfLargestSubset);
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/RepeatedString/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RepeatedString
+{
+    public class Startup
+    {
+        private static long CountNumberOfLettersAInString(string word, long count)
+        {
+            long counter = 0;
+            int letterAInWord = 0;
+            long remainder = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    letterAInWord++;
+                }
+            }
+
+            for (int i = 0; i < count % word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    remainder++;
+                }
+            }
+
+            counter += (count / word.Length) * letterAInWord + remainder;
+
+            return counter;
+        }
+
+        public static void Main()
+        {
+            string word = Console.ReadLine();
+            long count = long.Parse(Console.ReadLine());
+
+            long countNumberOfLettersAInString = CountNumberOfLettersAInString(word, count);
+            Console.WriteLine(countNumberOfLettersAInString);
+        }
+    }
+}
