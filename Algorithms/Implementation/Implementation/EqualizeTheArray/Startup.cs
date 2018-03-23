effead68dfaf7c37220c45172e9d7614b603bb2a
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-03-23 09:19:44 +0200

    Added EqualizeTheArray and JumpingOnTheClouds tasks

---
+++ Algorithms/Implementation/Implementation/EqualizeTheArray/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualizeTheArray
+{
+    public class Startup
+    {
+        private static int MinimumDeletedValues(int numberOfElements, int[] numbers)
+        {
+            List<int> listOfNumbers = new List<int>(numbers);
+
+            int mostCommonElement = listOfNumbers
+                         .GroupBy(n => n)
+                         .OrderByDescending(gr => gr.Count())
+                         .Take(1)
+                         .Select(gr => gr.Key)
+                         .ToList()[0];
+
+            listOfNumbers.RemoveAll(n => n != mostCommonElement);
+
+            return numbers.Length - listOfNumbers.Count();
+        }
+
+        public static void Main()
+        {
+            int numberOfElements = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int minimumDeletedValues = MinimumDeletedValues(numberOfElements, numbers);
+            Console.WriteLine(minimumDeletedValues);
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/JumpingOnTheClouds/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace JumpingOnTheClouds
+{
+    public class Startup
+    {
+        private static int MinimumNumberOfJumps(int numberOfClouds, int[] clouds)
+        {
+            int counter = -1;
+
+            for (int i = 0; i < numberOfClouds; i++)
+            {
+                if (i + 2 < numberOfClouds && clouds[i + 2] == 0)
+                {
+                    i++;
+                }
+
+                counter++;
+            }
+
+            return counter;
+        }
+
+        public static void Main()
+        {
+            int numberOfClouds = int.Parse(Console.ReadLine());
+            int[] clouds = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int minimumNumberOfJumps = MinimumNumberOfJumps(numberOfClouds, clouds);
+            Console.WriteLine(minimumNumberOfJumps);
+        }
+    }
+}
