be741fc18587aedfde1f6ce3328c8417b6b5171f
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-03-28 23:31:18 +0300

    Added ACMICPCTeam and TaumAndB'day tasks

---
+++ Algorithms/Implementation/Implementation/ACMICPCTeam/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ACMICPCTeam
+{
+    public class Startup
+    {
+        private static int[] MaximumNumberOfTopicsATeamCanKnow(string[] topics, int numberOfTopics)
+        {
+            int[] result = new int[2];
+            List<int> combinations = new List<int>();
+            int counter = 0;
+
+            for (int i = 0; i < topics.Length - 1; i++)
+            {
+                string currentTopic = topics[i];
+                for (int j = i + 1; j < topics.Length; j++)
+                {
+                    string nextTopic = topics[j];
+                    for (int k = 0; k < numberOfTopics; k++)
+                    {
+                        if (currentTopic[k] == '1' || nextTopic[k] == '1')
+                        {
+                            counter++;
+                        }
+                    }
+
+                    combinations.Add(counter);
+                    counter = 0;
+                }
+            }
+
+            int maxNumberOfTopics = combinations.Max();
+            int countOfMaxNumberOfTopics = combinations.Where(c => c == maxNumberOfTopics).Count();
+
+            result[0] = maxNumberOfTopics;
+            result[1] = countOfMaxNumberOfTopics;
+
+            return result;
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numberOfAttendees = input[0];
+            int numberOfTopics = input[1];
+
+            string[] topics = new string[numberOfAttendees];
+            for (int i = 0; i < numberOfAttendees; i++)
+            {
+                string topic = Console.ReadLine();
+                topics[i] = topic;
+            }
+
+            int[] result = MaximumNumberOfTopicsATeamCanKnow(topics, numberOfTopics);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/TaumAndB'day/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace TaumAndB_day
+{
+    public class Startup
+    {
+        private static long MinimumAmountOfUnitsToSpend(long blackGifts, long whiteGifts, long xUnits, long yUnits, long zUnits)
+        {
+            if (blackGifts * xUnits > blackGifts * (yUnits + zUnits))
+            {
+                return blackGifts * (yUnits + zUnits) + whiteGifts * yUnits;
+            }
+            else if (whiteGifts * yUnits > whiteGifts * (xUnits + zUnits))
+            {
+                return blackGifts * xUnits + whiteGifts * (xUnits + zUnits); 
+            }
+            else
+            {
+                return blackGifts * xUnits + whiteGifts * yUnits;
+            }
+        }
+
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                long[] gifts = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                long blackGifts = gifts[0];
+                long whiteGifts = gifts[1];
+
+                long[] costs = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+                long xUnits = costs[0];
+                long yUnits = costs[1];
+                long zUnits = costs[2];
+
+                long minimumAmountOfUnitsToSpend = MinimumAmountOfUnitsToSpend(blackGifts, whiteGifts, xUnits, yUnits, zUnits);
+                Console.WriteLine(minimumAmountOfUnitsToSpend);
+            }
+        }
+    }
+}
