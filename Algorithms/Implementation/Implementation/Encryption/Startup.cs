05428899dfa42845cab614fc1ae0f19fc96f7518
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-03-29 10:26:42 +0300

    Added Encryption and OrganizingContainersOfBalls tasks

---
+++ Algorithms/Implementation/Implementation/Encryption/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace Encryption
+{
+    public class Startup
+    {
+        private static string Encrypt(string sentence)
+        {
+            int length = sentence.Length;
+            int sqrtOtLength = (int)Math.Sqrt(length);
+            int rows = sqrtOtLength;
+            int counter = 0;
+
+            int cols;
+            int numberOfElementsToTake;
+
+            if (rows * rows == length)
+            {
+                cols = rows;
+                numberOfElementsToTake = cols;
+            }
+            else
+            {
+                cols = sqrtOtLength + 1;
+                numberOfElementsToTake = cols;
+            }
+
+            string[] grid = new string[rows];
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (counter + numberOfElementsToTake >= sentence.Length)
+                {
+                    numberOfElementsToTake = sentence.Length - counter;
+                }
+
+                grid[row] = sentence.Substring(counter, numberOfElementsToTake);
+                counter += numberOfElementsToTake;
+            }
+
+            string remainder = "";
+
+            if (sentence.Length - counter > 0)
+            {
+                remainder = sentence.Substring(counter, sentence.Length - counter);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int col = 0; col < cols; col++)
+            {
+                for (int rowGrid = 0; rowGrid < grid.Length; rowGrid++)
+                {
+                    string word = grid[rowGrid];
+                    
+                    if (col < word.Length)
+                    {
+                        char letter = word[col];
+                        sb.Append(letter);
+                    }
+                }
+
+                if (remainder != "" && col < remainder.Length)
+                {
+                    sb.Append(remainder[col]);
+                }
+
+                sb.Append(' ');
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static void Main()
+        {
+            // wclwfoznbmyycxvaxagjhtexdkwjqhlojykopldsxesbbnezqmixfpujbssrbfhlgubvfhpfliimvmnny
+            // wmgjpnull cyjqlejgi lyhhdzbui wctlsqsbm fxeoxmsvv ovxjeirfm zadysxbhn nxkkbffpn bawobphfy
+            string sentence = Console.ReadLine();
+            string encryptedSentence = Encrypt(sentence);
+
+            Console.WriteLine(encryptedSentence);
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/OrganizingContainersOfBalls/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace OrganizingContainersOfBalls
+{
+    public class Startup
+    {
+        private static string OrganizeContainers(int numberOfBallTypes)
+        {
+            int[] capacityOfBoxes = new int[numberOfBallTypes];
+            int[] totalQuantityOfEachBallType = new int[numberOfBallTypes];
+
+            for (int i = 0; i < numberOfBallTypes; i++)
+            {
+                int[] rowOfBowls = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int j = 0; j < numberOfBallTypes; j++)
+                {
+                    int ball = rowOfBowls[j];
+                    capacityOfBoxes[i] += ball;
+                    totalQuantityOfEachBallType[j] += ball;
+                }
+            }
+
+            Array.Sort(capacityOfBoxes);
+            Array.Sort(totalQuantityOfEachBallType);
+
+            if (Enumerable.SequenceEqual(capacityOfBoxes, totalQuantityOfEachBallType))
+            {
+                return "Possible";
+            }
+            else
+            {
+                return "Impossible";
+            }
+        }
+
+        public static void Main()
+        {
+            int numberOfQueries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfQueries; i++)
+            {
+                int numberOfBallTypes = int.Parse(Console.ReadLine());
+                string organizeContainers = OrganizeContainers(numberOfBallTypes);
+
+                Console.WriteLine(organizeContainers);
+            }
+        }
+    }
+}
