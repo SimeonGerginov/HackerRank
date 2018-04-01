4dd9d049172eb42d1c5ede0bf0304473fa10c6ba
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-04-01 20:25:12 +0300

    Added BiggerIsGreater and ModifiedKaprekarNumbers tasks

---
+++ Algorithms/Implementation/Implementation/BiggerIsGreater/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace BiggerIsGreater
+{
+    public class Startup
+    {
+        private static string BiggerIsGreater(string word)
+        {
+            int firstIndex = FindFirstIndex(word);
+
+            if (firstIndex == -1)
+            {
+                return "no answer";
+            }
+
+            int secondIndex = FindSecondIndex(word, firstIndex);
+
+            string wordWithSwappedCharacters = SwapCharactersInWord(word, firstIndex, secondIndex);
+
+            string firstPartOfWord = wordWithSwappedCharacters.Substring(0, firstIndex + 1);
+            char[] secondPartOfWord = wordWithSwappedCharacters.Substring(firstIndex + 1, wordWithSwappedCharacters.Length - firstIndex - 1).Reverse().ToArray();
+
+            return firstPartOfWord + new string(secondPartOfWord);
+        }
+
+        private static int FindFirstIndex(string word)
+        {
+            int index = -1;
+
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] < word[i + 1])
+                {
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+
+        private static int FindSecondIndex(string word, int firstIndex)
+        {
+            int anotherIndex = -1;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] > word[firstIndex])
+                {
+                    anotherIndex = i;
+                }
+            }
+
+            return anotherIndex;
+        }
+
+        private static string SwapCharactersInWord(string word, int firstPosition, int secondPosition)
+        {
+            char[] wordAsCharArray = word.ToCharArray();
+
+            char tempChar = wordAsCharArray[firstPosition]; 
+            wordAsCharArray[firstPosition] = wordAsCharArray[secondPosition];
+            wordAsCharArray[secondPosition] = tempChar;
+           
+            return new string(wordAsCharArray);
+        }
+
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string word = Console.ReadLine();
+                string newWord = BiggerIsGreater(word);
+
+                Console.WriteLine(newWord);
+            }
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/ModifiedKaprekarNumbers/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModifiedKaprekarNumbers
+{
+    public class Startup
+    {
+        private static List<int> FindKaprekarNumbersInRange(int startOfRange, int endOfRange)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int number = startOfRange; number <= endOfRange; number++)
+            {
+                if (IsKaprekarNumber(number))
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            return numbers;
+        }
+
+        private static bool IsKaprekarNumber(long number)
+        {
+            if (number == 1)
+            {
+                return true;
+            }
+
+            long squareOfNumber = number * number;
+            int countOfDigits = squareOfNumber.ToString().Length;
+            int countOfDigitsOfNumber = number.ToString().Length;
+
+            for (int rightDigits = 1; rightDigits < countOfDigits; rightDigits++)
+            {
+                long currentParts = (int)Math.Pow(10, rightDigits);
+
+                if (currentParts == number)
+                {
+                    continue;
+                }
+
+                long sum = squareOfNumber / currentParts + squareOfNumber % currentParts;
+                int rightPartDigitsCount = countOfDigits - (squareOfNumber / currentParts).ToString().Length;
+
+                if (sum == number && countOfDigitsOfNumber == rightPartDigitsCount)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void Main()
+        {
+            int startOfRange = int.Parse(Console.ReadLine());
+            int endOfRange = int.Parse(Console.ReadLine());
+
+            List<int> numbers = FindKaprekarNumbersInRange(startOfRange, endOfRange);
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            else
+            {
+                Console.WriteLine("INVALID RANGE");
+            }
+        }
+    }
+}
