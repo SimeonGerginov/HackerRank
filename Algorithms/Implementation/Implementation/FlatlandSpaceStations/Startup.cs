0059de6ec93c1572259854d0b98d64b795e13670
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-04-04 17:46:55 +0300

    Added FlatlandSpaceStations and Lisa'sWorkbook tasks

---
+++ Algorithms/Implementation/Implementation/FlatlandSpaceStations/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace FlatlandSpaceStations
+{
+    public class Startup
+    {
+        private static int MaximumDistance(int cities, int citiesWithStations, int[] indexesOfCitiesWithStations)
+        {
+            if (cities == citiesWithStations)
+            {
+                return 0;
+            }
+
+            Array.Sort(indexesOfCitiesWithStations);
+            int maxDistance = indexesOfCitiesWithStations[0];
+
+            for (int i = 1; i < citiesWithStations; i++)
+            {
+                int distance = (indexesOfCitiesWithStations[i] - indexesOfCitiesWithStations[i - 1]) / 2;
+
+                if (distance > maxDistance)
+                {
+                    maxDistance = distance;
+                }
+            }
+
+            int lastSpaceStationDistance = cities - 1 - indexesOfCitiesWithStations[citiesWithStations - 1];
+
+            if (lastSpaceStationDistance > maxDistance)
+            {
+                maxDistance = lastSpaceStationDistance;
+            }
+
+            return maxDistance;
+        }
+
+        private static int MaximumDistance2(int cities, int citiesWithStations, int[] indexesOfCitiesWithStations)
+        {
+            if (cities == citiesWithStations)
+            {
+                return 0;
+            }
+
+            int[] distances = new int[cities];
+
+            for (int i = 0; i < cities; i++)
+            {
+                if (indexesOfCitiesWithStations.Contains(i))
+                {
+                    distances[i] = 0;
+                }
+                else
+                {
+                    int minDistance = int.MaxValue;
+                    for (int j = 0; j < citiesWithStations; j++)
+                    {
+                        int currentDistance = Math.Abs(indexesOfCitiesWithStations[j] - i);
+
+                        if (currentDistance < minDistance)
+                        {
+                            minDistance = currentDistance;
+                        }
+                    }
+
+                    distances[i] = minDistance;
+                }
+            }
+
+            return distances.Max();
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int cities = input[0];
+            int citiesWithStations = input[1];
+
+            int[] indexesOfCitiesWithStations = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int maximumDistance = MaximumDistance(cities, citiesWithStations, indexesOfCitiesWithStations);
+            Console.WriteLine(maximumDistance);
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/Lisa'sWorkbook/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lisa_sWorkbook
+{
+    public class Startup
+    {
+        private static int NumberOfSpecialProblems(int[] problemsInChapters, int numberOfChapters, int maximumNumberOfProblemsPerPage)
+        {
+            int numberOfSpecialProblems = 0;
+            int currentPage = 1;
+
+            for (int i = 0; i < numberOfChapters; i++)
+            {
+                int currentChapter = problemsInChapters[i];
+                int startOfRange = 1;
+                while (true)
+                {
+                    if (currentChapter <= 0)
+                    {
+                        break;
+                    }
+
+                    List<int> numbers;
+                    if (currentChapter > maximumNumberOfProblemsPerPage)
+                    {
+                        numbers = Enumerable.Range(startOfRange, maximumNumberOfProblemsPerPage).ToList();
+                    }
+                    else
+                    {
+                        numbers = Enumerable.Range(startOfRange, currentChapter).ToList();
+                    }
+
+                    currentChapter -= maximumNumberOfProblemsPerPage;
+
+                    if (numbers.Contains(currentPage))
+                    {
+                        numberOfSpecialProblems++;
+                    }
+
+                    startOfRange += maximumNumberOfProblemsPerPage;
+                    currentPage++;
+                }
+            }
+
+            return numberOfSpecialProblems;
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numberOfChapters = input[0];
+            int maximumNumberOfProblemsPerPage = input[1];
+
+            int[] problemsInChapters = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int numberOfSpecialProblems = NumberOfSpecialProblems(problemsInChapters, numberOfChapters, maximumNumberOfProblemsPerPage);
+            Console.WriteLine(numberOfSpecialProblems);
+        }
+    }
+}
