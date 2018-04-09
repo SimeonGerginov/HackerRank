b8ba02d802511a9ed300e7e1a1ae0652fadf0f05
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-04-09 11:22:02 +0300

    Added AbsolutePermutation and TheBombermanGame tasks

---
+++ Algorithms/Implementation/Implementation/AbsolutePermutation/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbsolutePermutation
+{
+    public class Startup
+    {
+        private static List<int> AbsolutePermutations(int n, int k)
+        {
+            List<int> absolutePermutations = new List<int>();
+
+            if (k == 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    absolutePermutations.Add(i);
+                }
+            }
+            else if (n % (2 * k) != 0)
+            {
+                absolutePermutations.Add(-1);
+            }
+            else
+            {
+                bool shouldAdd = true;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (shouldAdd)
+                    {
+                        absolutePermutations.Add(i + k);
+                    }
+                    else
+                    {
+                        absolutePermutations.Add(i - k);
+                    }
+
+                    if (i % k == 0)
+                    {
+                        if (shouldAdd)
+                        {
+                            shouldAdd = false;
+                        }
+                        else
+                        {
+                            shouldAdd = true;
+                        }
+                    }
+                }
+            }
+
+            return absolutePermutations;
+        }
+
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                int n = input[0];
+                int k = input[1];
+
+                List<int> absolutePermutations = AbsolutePermutations(n, k);
+                Console.WriteLine(string.Join(" ", absolutePermutations));
+            }
+        }
+    }
+}
---
+++ Algorithms/Implementation/Implementation/TheBombermanGame/Startup.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+
+namespace TheBombermanGame
+{
+    public class Startup
+    {
+        private static void PrintGrid(char[,] grid, int rows, int cols)
+        {
+            for (int currentRow = 0; currentRow < rows; currentRow++)
+            {
+                for (int currentCol = 0; currentCol < cols; currentCol++)
+                {
+                    Console.Write(grid[currentRow, currentCol]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static char[,] GridAfterEvenSeconds(int rows, int cols)
+        {
+            char[,] grid = new char[rows, cols];
+
+            for (int currentRow = 0; currentRow < rows; currentRow++)
+            {
+                for (int currentCol = 0; currentCol < cols; currentCol++)
+                {
+                    grid[currentRow, currentCol] = 'O';
+                }
+            }
+
+            return grid;
+        }
+
+        private static char[,] GridAfterSeconds(char[,] grid, int rows, int cols)
+        {
+            for (int currentRow = 0; currentRow < rows; currentRow++)
+            {
+                for (int currentCol = 0; currentCol < cols; currentCol++)
+                {
+                    char currentCell = grid[currentRow, currentCol];
+
+                    if (currentCell == 'O')
+                    {
+                        grid[currentRow, currentCol] = ',';
+                    }
+                    else
+                    {
+                        grid[currentRow, currentCol] = 'O';
+                    }
+                }
+            }
+
+            for (int currentRow = 0; currentRow < rows; currentRow++)
+            {
+                for (int currentCol = 0; currentCol < cols; currentCol++)
+                {
+                    char currentCell = grid[currentRow, currentCol];
+
+                    if (currentCell == ',')
+                    {
+                        grid[currentRow, currentCol] = '.';
+
+                        if (currentRow > 0 && grid[currentRow - 1, currentCol] != ',')
+                        {
+                            grid[currentRow - 1, currentCol] = '.';
+                        }
+
+                        if (currentRow < rows - 1 && grid[currentRow + 1, currentCol] != ',')
+                        {
+                            grid[currentRow + 1, currentCol] = '.';
+                        }
+
+                        if (currentCol > 0 && grid[currentRow, currentCol - 1] != ',')
+                        {
+                            grid[currentRow, currentCol - 1] = '.';
+                        }
+
+                        if (currentCol < cols - 1 && grid[currentRow, currentCol + 1] != ',')
+                        {
+                            grid[currentRow, currentCol + 1] = '.';
+                        }
+                    }
+                }
+            }
+
+            return grid;
+        }
+
+        private static char[,] GridStateAfterNSeconds(char[,] grid, int rows, int cols, long seconds)
+        {
+            if (seconds == 1)
+            {
+                return grid;
+            }
+            else if (seconds % 2 == 0)
+            {
+                char[,] newGrid = GridAfterEvenSeconds(rows, cols);
+
+                return newGrid;
+            }
+            else
+            {
+                while (true)
+                {
+                    if (seconds == 3 || seconds == 5)
+                    {
+                        break;
+                    }
+
+                    seconds -= 4;
+                }
+
+                if (seconds == 3)
+                {
+                    // after 3, 7, 11 and ... seconds
+                    char[,] newGrid = GridAfterSeconds(grid, rows, cols);
+
+                    return newGrid;
+                }
+                else
+                {
+                    // seconds == 5
+                    // after 5, 9, 14 and ... seconds
+                    char[,] gridAfterThreeSeconds = GridAfterSeconds(grid, rows, cols);
+                    char[,] newGrid = GridAfterSeconds(gridAfterThreeSeconds, rows, cols);
+
+                    return newGrid;
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            long seconds = (long)input[2];
+
+            char[,] grid = new char[rows, cols];
+
+            for (int currentRow = 0; currentRow < rows; currentRow++)
+            {
+                string row = Console.ReadLine();
+                for (int currentCol = 0; currentCol < cols; currentCol++)
+                {
+                    grid[currentRow, currentCol] = row[currentCol];
+                }
+            }
+
+            char[,] gridStateAfterNSeconds = GridStateAfterNSeconds(grid, rows, cols, seconds);
+
+            PrintGrid(gridStateAfterNSeconds, rows, cols);
+        }
+    }
+}
