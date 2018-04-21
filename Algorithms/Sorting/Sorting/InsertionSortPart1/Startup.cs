e4fd1d624ec703a1842a2d685b330fb19b5a0e4f
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-04-21 12:08:16 +0300

    Added IntroToTutorialChallenges and InsertionSortPart1 tasks

---
+++ Algorithms/Sorting/Sorting/InsertionSortPart1/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace InsertionSortPart1
+{
+    public class Startup
+    {
+        private static void InsertionSort(int[] numbers, int sizeOfArray)
+        {
+            int unsortedElement = numbers[sizeOfArray - 1];
+            for (int i = sizeOfArray - 2; i >= 0; i--)
+            {
+                if (numbers[i] > unsortedElement)
+                {
+                    numbers[i + 1] = numbers[i];
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+                else
+                {
+                    numbers[i + 1] = unsortedElement;
+                    Console.WriteLine(string.Join(" ", numbers));
+                    break;
+                }
+            }
+
+            if (!numbers.Contains(unsortedElement))
+            {
+                numbers[0] = unsortedElement;
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+
+        public static void Main()
+        {
+            int sizeOfArray = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            InsertionSort(numbers, sizeOfArray);
+        }
+    }
+}
---
+++ Algorithms/Sorting/Sorting/IntroToTutorialChallenges/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroToTutorialChallenges
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string number = Console.ReadLine();
+            int size = int.Parse(Console.ReadLine());
+            string[] arrayOfNumbers = Console.ReadLine().Split(' ');
+
+            int indexOfNumber = Array.IndexOf(arrayOfNumbers, number);
+            Console.WriteLine(indexOfNumber);
+        }
+    }
+}
