d2b6f64450d307089c1909e48f429a361c404914
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-04-21 11:30:03 +0300

    Added Anagram and MakingAnagrams tasks

---
+++ Algorithms/Strings/Strings/Anagram/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Anagram
+{
+    public class Startup
+    {
+        private static int MinimumNumberOfChanges(string currentWord)
+        {
+            string leftPart = currentWord.Substring(0, currentWord.Length / 2);
+            string rightPart = currentWord.Substring(currentWord.Length / 2);
+
+            if (leftPart.Length != rightPart.Length)
+            {
+                return -1;
+            }
+
+            int commonCharacters = 0;
+
+            string tempString = rightPart;
+            for (int i = 0; i < leftPart.Length; i++)
+            {
+                int indexOfCharacter = tempString.IndexOf(leftPart[i]);
+                if (indexOfCharacter > -1)
+                {
+                    tempString = tempString.Remove(indexOfCharacter, 1);
+                    commonCharacters++;
+                }
+            }
+
+            return leftPart.Length - commonCharacters;
+        }
+
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentWord = Console.ReadLine();
+
+                int minimumNumberOfChanges = MinimumNumberOfChanges(currentWord);
+                Console.WriteLine(minimumNumberOfChanges);
+            }
+        }
+    }
+}
---
+++ Algorithms/Strings/Strings/MakingAnagrams/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MakingAnagrams
+{
+    public class Startup
+    {
+        private static int MinimumNumberOfCharactersToDelete(string firstString, string secondString)
+        {
+            int commonCharacters = 0;
+
+            if (firstString.Length <= secondString.Length)
+            {
+                string tempString = secondString;
+                for (int i = 0; i < firstString.Length; i++)
+                {
+                    int indexOfCharacter = tempString.IndexOf(firstString[i]);
+                    if (indexOfCharacter > -1)
+                    {
+                        tempString = tempString.Remove(indexOfCharacter, 1);
+                        commonCharacters++;
+                    }
+                }
+            }
+            else
+            {
+                string tempString = firstString;
+                for (int i = 0; i < secondString.Length; i++)
+                {
+                    int indexOfCharacter = tempString.IndexOf(secondString[i]);
+                    if (indexOfCharacter > -1)
+                    {
+                        tempString = tempString.Remove(indexOfCharacter, 1);
+                        commonCharacters++;
+                    }
+                }
+            }
+
+            return (firstString.Length - commonCharacters) + (secondString.Length - commonCharacters);
+        }
+
+        public static void Main()
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+
+            int minimumNumberOfCharactersToDelete = MinimumNumberOfCharactersToDelete(firstString, secondString);
+            Console.WriteLine(minimumNumberOfCharactersToDelete);
+        }
+    }
+}
