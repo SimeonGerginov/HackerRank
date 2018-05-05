08b8d366e60828b0007b87826db399c36a48dacf
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-05-05 08:25:54 +0300

    Added LuckBalance and MaximumPerimeterTriangle tasks

---
+++ Algorithms/Greedy/Greedy/LuckBalance/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LuckBalance
+{
+    public class Startup
+    {
+        private static int MaximumNumberOfLuck(ICollection<int> importantContests, ICollection<int> notImportantContests, 
+            int maxNumberOfContestsToLose)
+        {
+            int maximumNumberOfLuck = 0;
+            int contestsToLose = importantContests.Count - maxNumberOfContestsToLose;
+
+            if (contestsToLose <= 0)
+            {
+                maximumNumberOfLuck += importantContests.Sum() + notImportantContests.Sum();
+            }
+            else
+            {
+                importantContests = importantContests.OrderBy(c => c).ToList();
+                maximumNumberOfLuck += importantContests.Sum();
+
+                importantContests = importantContests.Take(contestsToLose).ToList();
+                maximumNumberOfLuck -= importantContests.Sum() * 2;
+
+                maximumNumberOfLuck += notImportantContests.Sum();
+            }
+
+            return maximumNumberOfLuck;
+        }
+
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numberOfContests = input[0];
+            int maxNumberOfContestsToLose = input[1];
+
+            ICollection<int> importantContests = new List<int>();
+            ICollection<int> notImportantContests = new List<int>();
+
+            for (int i = 0; i < numberOfContests; i++)
+            {
+                input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                int luckBalance = input[0];
+                int importanceRating = input[1];
+
+                if (importanceRating == 1)
+                {
+                    importantContests.Add(luckBalance);
+                }
+                else
+                {
+                    notImportantContests.Add(luckBalance);
+                }
+            }
+
+            int maximumNumberOfLuck = MaximumNumberOfLuck(importantContests, notImportantContests, maxNumberOfContestsToLose);
+            Console.WriteLine(maximumNumberOfLuck);
+        }
+    }
+}
---
+++ Algorithms/Greedy/Greedy/MaximumPerimeterTriangle/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumPerimeterTriangle
+{
+    public class Startup
+    {
+        private static IList<int> SidesOfTriangle(int[] sticks, int numberOfSticks)
+        {
+            Array.Sort(sticks);
+            IList<int> sides = new List<int>();
+            int index = numberOfSticks - 3;
+
+            while (index >= 0 && (sticks[index] + sticks[index + 1] <= sticks[index + 2]))
+            {
+                index--;
+            }
+
+            if (index >= 0)
+            {
+                sides.Add(sticks[index]);
+                sides.Add(sticks[index + 1]);
+                sides.Add(sticks[index + 2]);
+
+                return sides;
+            }
+            else
+            {
+                sides.Add(-1);
+
+                return sides;
+            }
+        }
+
+        public static void Main()
+        {
+            int numberOfSticks = int.Parse(Console.ReadLine());
+            int[] sticks = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            IList<int> sidesOfTriangle = SidesOfTriangle(sticks, numberOfSticks);
+            Console.WriteLine(string.Join(" ", sidesOfTriangle));
+        }
+    }
+}
