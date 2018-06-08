9d6aba357ac72e35f2def7956a990925686ef5dc
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-08 18:00:32 +0300

    Added StockMaximize task

---
+++ Algorithms/DynamicProgramming/DynamicProgramming/StockMaximize/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace StockMaximize
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numberOfTestCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfTestCases; i++)
+            {
+                int numberOfPredictedPrices = int.Parse(Console.ReadLine());
+                int[] prices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                long maximumProfit = MaxProfit(prices, numberOfPredictedPrices);
+                Console.WriteLine(maximumProfit);
+            }
+        }
+
+        private static long MaxProfit(int[] prices, int numberOfPredictedPrices)
+        {
+            long maxProfit = 0;
+            int currentMax = 0;
+
+            for (int i = numberOfPredictedPrices - 1; i >= 0; i--)
+            {
+                if (prices[i] >= currentMax)
+                {
+                    currentMax = prices[i];
+                }
+
+                maxProfit += currentMax - prices[i];
+            }
+
+            return maxProfit;
+        }
+    }
+}
